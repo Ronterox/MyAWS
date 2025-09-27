175c2c86ebe271303df8b286b16d606bae0aaffb
Ricardo <ricarditomontserrat@gmail.com>
2025-09-27 20:49:04 +0200

    more testing, after upgrading the fyne version and now it can handle a list much better

--- main.go
+++ main.go
@@ -1,66 +1,85 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/storage"
-	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Toolbar Widget")
-
-	input := widget.NewMultiLineEntry()
-	input.SetPlaceHolder("Enter text here...")
-
-	getSelected := func(action string) string {
-		log.Println(action)
-
-		selected := input.SelectedText()
-		if selected == "" {
-			selected = input.Text
-		}
-		return selected
-	}
-
-	toolbar := widget.NewToolbar(
-		widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
-			log.Println("New document")
-
-            writer, err := storage.Writer(storage.NewFileURI("test.txt"))
-            if err != nil {
-                log.Println(err)
-                return
-            }
-
-            writer.Write([]byte(input.Text))
-		}),
-		widget.NewToolbarSeparator(),
-		widget.NewToolbarAction(theme.ContentCutIcon(), func() {
-			w.Clipboard().SetContent(getSelected("Cut"))
-			input.SetText("")
-		}),
-		widget.NewToolbarAction(theme.ContentCopyIcon(), func() {
-			w.Clipboard().SetContent(getSelected("Copy"))
-		}),
-		widget.NewToolbarAction(theme.ContentPasteIcon(), func() {
-			input.SetText(w.Clipboard().Content())
-		}),
-		widget.NewToolbarAction(theme.SearchReplaceIcon(), func() {}),
-		widget.NewToolbarSpacer(),
-		widget.NewToolbarAction(theme.WindowCloseIcon(), func() {
-			a.Quit()
-		}),
-		widget.NewToolbarAction(theme.HelpIcon(), func() {
-			log.Println("Display help")
-		}),
-	)
-
-	w.SetContent(container.NewBorder(toolbar, nil, nil, nil, input))
-	w.ShowAndRun()
+type User struct {
+	ID          int    `json:"id"`
+	Username    string `json:"nickname"`
+	ProfileLink string `json:"github_profile"`
 }
 
+func main() {
+	var users []User
+
+	a := app.New()
+	w := a.NewWindow("My AWS")
+
+	text := widget.NewLabel("Fetch data test")
+	data := binding.BindStringList(&[]string{})
+	list := widget.NewListWithData(data,
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template")
+		},
+		func(i binding.DataItem, o fyne.CanvasObject) {
+			o.(*widget.Label).Bind(i.(binding.String))
+		},
+	)
+	list.OnSelected = func(i widget.ListItemID) {
+		user := users[i]
+		path := strings.Replace(user.ProfileLink, "https://github.com/", "", 1)
+		url := url.URL{Scheme: "https", Host: "github.com", Path: path}
+		fyne.CurrentApp().OpenURL(&url)
+	}
+
+	content := container.NewBorder(
+		container.NewHBox(
+			text,
+			widget.NewButton("Fetch", func() {
+				text.SetText("Fetching data...")
+
+				go func() {
+					res, err := http.Get("https://24pullrequests.com/users.json?page=2")
+					if err != nil {
+						fmt.Printf("Error fetching data: %v", err)
+						return
+					}
+					defer res.Body.Close()
+
+					bytes, _ := io.ReadAll(res.Body)
+
+					if err := json.Unmarshal(bytes, &users); err != nil {
+						fmt.Printf("Error unmarshalling data: %v", err)
+						return
+					}
+
+					for _, user := range users {
+						data.Append(user.Username)
+					}
+
+					fyne.Do(func() {
+						text.SetText("Click to open the user profile on your browser")
+					})
+				}()
+			}),
+			widget.NewButton("Close", func() { a.Quit() }),
+		),
+		nil, nil, nil,
+		list,
+	)
+
+	w.SetContent(content)
+	w.ShowAndRun()
+}
